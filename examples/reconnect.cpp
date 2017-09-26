121f1005f33b5dec2c17f4497a01264000ed9512
Stéphane Magnenat <stephane@magnenat.net>
2017-09-26 17:42:05 +0200

    Added test code that attempts to reconnect a disconnected target.

---
+++ examples/reconnect.cpp
@@ -0,0 +1,93 @@
+#include <dashel/dashel.h>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+using namespace Dashel;
+
+class ReconnectingHub: public Hub
+{
+public:
+	ReconnectingHub(string target):
+		connected(false),
+		target(move(target))
+	{
+		connectToTarget();
+	}
+
+	void stepAndReconnect()
+	{
+		// first step
+		step(1000);
+		// if disconnected...
+		if (!connected)
+		{
+			// ...attempt to reconnect
+			connectToTarget();
+		}
+	}
+
+protected:
+	bool connected;
+	string target;
+
+protected:
+	void connectToTarget()
+	{
+		try
+		{
+			connect(target);
+			connected = true;
+		}
+		catch (const DashelException& e)
+		{
+			cout << endl << "Failed connecting to " << target << " : " << e.what() << endl;
+		}
+	}
+
+protected:
+	virtual void connectionCreated(Stream * stream)
+	{
+		cout << endl << "Connected to " << stream->getTargetName() << endl;
+	}
+
+	virtual void incomingData(Stream * stream)
+	{
+		char c = stream->read<char>();
+		cout << ".";
+	}
+
+	virtual void connectionClosed(Stream * stream, bool abnormal)
+	{
+		connected = false;
+		cout << endl << "Lost connection to " << stream->getTargetName();
+		if (abnormal)
+			cout << " : " << stream->getFailReason();
+		cout << endl;
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		if (argc > 1)
+		{
+			ReconnectingHub reconnectingHub(argv[1]);
+			while (true)
+			{
+				reconnectingHub.stepAndReconnect();
+			}
+		}
+		else
+		{
+			cerr << "Usage " << argv[0] << " target" << endl;
+		}
+	}
+	catch(const DashelException &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
